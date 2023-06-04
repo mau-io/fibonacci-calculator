8598b10913d331844350bde34053fdbbef2930c9
mau-io <maurice222@outlook.com>
2023-06-04 01:00:16 -0700

    Add react base app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Fibonacci from './components/Fibonacci';
+
+function App() {
+  return (
+    <div className="App">
+      <Fibonacci />
+    </div>
+  );
+}
+
+export default App;
